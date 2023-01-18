b509a1f4798b607043de4f27eed829ab5e5ad16e
My Name <email@email.com>
2023-01-17 20:25:01 -0800

    Added test code.

--- NestedObjects/Program.cs
+++ NestedObjects/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using NestedObjects;
+
+Advisor compSciAdvisor = new Advisor()
+{
+    Email = "name@email.edu",
+    FullName = "AdvisorFirstName AdvisorLastName",
+    OfficeLocation = "Building Number"
+
+};
+
+Student stu1 = new Student()
+{
+    FirstName = "StudentFirst",
+    LastName = "StudentLast",
+    DateOfBirth = new DateOnly(1899, 12, 31),
+    AssignedAdvisor = compSciAdvisor,
+    PhoneNumber = "1234567890",
+    SchoolEmail = "email@school.edu"
+};
+
+
+Console.WriteLine($"Student: {stu1.FirstName} has {stu1.AssignedAdvisor.FullName}");
